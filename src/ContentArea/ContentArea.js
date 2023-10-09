b6db3f6ee3c3fce65b7de0a95fb8053abd592dec
Ali-andria <AS.bizEnice@gmail.com>
2023-10-09 11:08:00 +0300

    my first commit

--- src/ContentArea/ContentArea.js
+++ src/ContentArea/ContentArea.js
@@ -1,28 +1,11 @@
-import React, { createContext, useState } from 'react'
-import { Product } from "../product";
+import React from 'react'
 
-export const ContentArea = createContext(null);
-const getDefaultCart = () => {
-    let cart = {}
-    for(let i= 1; i <PRODUCT.length +1; i++){
-        cart[i] = 0;
-    }
-  return cart;
-};
-export const ContentAreaProvider = (props) => {
-const [cartItems, setCartItems] = useState(getDefaultCart());
-const addToCart = (itemId) => {
-    setCartItems((prev)=>({...prev, [itemId]:prev[itemId] + 1}))
-};
-const removeFromCart = (itemId) => {
-    setCartItems((prev)=>({...prev, [itemId]:prev[itemId] - 1}))
-};
-const contextValue = {cartItems, addToCart, removeFromCart};
-// console.log(cartItems);
-    return(
-        <ContentArea.Provider value={contextValue}>
-            {props.children}
-        </ContentArea.Provider>
-        );
+const ContentArea = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
 
-};
+export default ContentArea
